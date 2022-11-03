f8b3d46f71dce5995f5abb6a175ea5b106c48c11
Sarah <sarahvictoria230@gmail.com>
2022-11-03 20:26:44 -0300

    Aula 4 - Refatoração, Criação de animações e outros estilos

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.4
 function createCard(date, day, games) {
+  delay = delay + 0.4
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -19,13 +21,13 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector('#app').innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-  <main id="cards">
-    ${createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia') + createGame('uruguay', '10: 00', 'south-korea'))}
-    ${createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland') + createGame('uruguay', '16:00', 'portugal'))}
-    ${createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon') + createGame('south-korea', '12:00', 'portugal'))}
-  </main>
-`
+document.querySelector('#cards').innerHTML =
+  createCard('24/11', 'quinta',
+    createGame('brazil', '16:00', 'serbia') +
+    createGame('uruguay', '10: 00', 'south-korea')) +
+
+  createCard('28/11', 'segunda',
+    createGame('brazil', '13:00', 'switzerland') + createGame('uruguay', '16:00', 'portugal')) +
+
+  createCard('02/12', 'sexta',
+    createGame('brazil', '16:00', 'cameroon') + createGame('south-korea', '12:00', 'portugal'))
